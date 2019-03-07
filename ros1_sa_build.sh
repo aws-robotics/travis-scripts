36233858aadf8886a8645e6f9a1d45ab2fd6f1f5
AAlon <avishayalon@gmail.com>
2019-03-06 20:52:34 -0800

    Update ros1_sa_build.sh

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -9,8 +9,9 @@ pip3 install colcon-bundle colcon-ros-bundle
 
 ls /"$ROS_DISTRO"_ws/src
 bash -c "echo '`ls /"$ROS_DISTRO"_ws/src 2>&1`'"
+BUILD_DIR_NAME=`basename $TRAVIS_BUILD_DIR`
 # use colcon as build tool to build the robot workspace
-cd /"$ROS_DISTRO"_ws/src/robot_ws/
+cd /"$ROS_DISTRO"_ws/src/$BUILD_DIR_NAME/robot_ws/
 rosws update
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base build --install-base install
@@ -18,7 +19,7 @@ colcon bundle --build-base build --install-base install --bundle-base bundle
 mv ./bundle/output.tar.gz ./bundle/robot.tar.gz
 
 # use colcon as build tool to build the simulation workspace
-cd /"$ROS_DISTRO"_ws/src/simulation_ws/
+cd /"$ROS_DISTRO"_ws/src/$BUILD_DIR_NAME/simulation_ws/
 rosws update
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base build --install-base install
@@ -26,5 +27,5 @@ colcon bundle --build-base build --install-base install --bundle-base bundle
 mv ./bundle/output.tar.gz ./bundle/simulation.tar.gz
 
 # move the artifacts to a shared mount point
-mv /"$ROS_DISTRO"_ws/src/robot_ws/bundle/robot.tar.gz /shared/robot.tar.gz
-mv /"$ROS_DISTRO"_ws/src/simulation_ws/bundle/simulation.tar.gz /shared/simulation.tar.gz
+mv /"$ROS_DISTRO"_ws/src/$BUILD_DIR_NAME/robot_ws/bundle/robot.tar.gz /shared/robot.tar.gz
+mv /"$ROS_DISTRO"_ws/src/$BUILD_DIR_NAME/simulation_ws/bundle/simulation.tar.gz /shared/simulation.tar.gz
