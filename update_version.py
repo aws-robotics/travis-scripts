b0933a7034b0930f1ce55814e4b4d4eacc5a1c86
AAlon <avishayalon@gmail.com>
2019-03-06 18:06:50 -0800

    Update update_version.py

--- update_version.py
+++ update_version.py
@@ -1,5 +1,12 @@
 import os
+import sys
 
-if not os.path.exists('version.json'):
-    with open('version.json', 'wb') as f:
-        f.write('{"application_version": "1.0.0"}\n')
+def update_version(file_path):
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            f.write('{"application_version": "1.0.0"}\n')
+        
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise ValueError("Missing version file path parameter")
+    update_version(sys.argv[1])
