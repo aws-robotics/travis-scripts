eab2c177ad98a00aec9244ebb088132fd80e4f6b
AAlon <avishayalon@gmail.com>
2019-03-06 21:52:31 -0800

    Update update_version.py

--- update_version.py
+++ update_version.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import subprocess
+import xml.etree.ElementTree as ET
+from distutils.version import StrictVersion
 
 VERSION_COMPONENTS_TO_IDX = {'major': 0, 'minor': 1, 'patch': 2}
 FIRST_VERSION = '1.0.0'
@@ -17,12 +20,40 @@ def version_increment(version, increment_type='patch'):
         components[VERSION_COMPONENTS_TO_IDX[increment_type]] = str(int(components[VERSION_COMPONENTS_TO_IDX[increment_type]]) + 1)
         return '.'.join(components)
 
-def update_version(file_path, current_version):
+def update_version(file_path, current_version, package_name):
     new_version = version_increment(current_version)
+    # Create ephemeral version.json in order to tell the internal build system which version should be built.
     with open(file_path, 'wb') as f:
         f.write('{"application_version": "%s"}\n' % (new_version, ))
+    # Update the version in package.xml if not up to date & commit
+    package_xml_path = get_path_to_package_xml()
+    if not package_xml_path:
+        # Skip updating package.xml
+        return
+    xml_tree = ET.parse(package_xml_path)
+    xml_root = xml_tree.getroot()
+    version_element = root.findall('version')[0]
+    if StrictVersion(version_element.text) == StrictVersion(new_version):
+        print 'package.xml already up to date with version %s, not updating.' % (new_version, )
+    elif StrictVersion(version_element.text) > StrictVersion(new_version):
+        raise ValueError('The version in package.xml (%s) is newer than the version intended to be released (%s)' % (version_element.text, new_version, ))
+    else:
+        version_element.text = new_version
+        tree.write(package_xml_path)
+    # TODO: git commit, push / open PR.
+        
+def get_path_to_package_xml():
+    """
+    @note assumes the environment contains TRAVIS_BUILD_DIR and SA_PACKAGE_NAME.
+    """
+    if 'TRAVIS_BUILD_DIR' in os.environ and 'SA_PACKAGE_NAME' in os.environ:
+        search_cmd = 'find %s -name "package.xml" -path "*/%s/*"' % (os.environ['TRAVIS_BUILD_DIR'], os.environ['SA_PACKAGE_NAME'], )
+        return subprocess.check_output(search_cmd, shell=True).strip()
+    else:
+        return None
         
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        raise ValueError("update_version.py <version file path> <current version>")
-    update_version(sys.argv[1], sys.argv[2])
+        raise ValueError("Missing parameters. Usage: update_version.py <version file path> <current version>")
+    version_file_path, current_version = sys.argv[1:]
+    update_version(version_file_path, current_version)
