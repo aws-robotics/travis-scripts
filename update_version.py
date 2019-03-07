f424320abec2b582e5e965c3f8ef692cdcd72cfa
AAlon <avishayalon@gmail.com>
2019-03-06 18:29:25 -0800

    Update update_version.py

--- update_version.py
+++ update_version.py
@@ -1,12 +1,19 @@
 import os
 import sys
 
-def update_version(file_path):
-    if not os.path.exists(file_path):
-        with open(file_path, 'wb') as f:
-            f.write('{"application_version": "1.0.0"}\n')
+VERSION_COMPONENTS_TO_IDX = {'major': 0, 'minor': 1, 'patch': 2}
+
+def version_increment(version, increment_type='patch'):
+    components = version.split('.')
+    components[VERSION_COMPONENTS_TO_IDX[increment_type]] = str(int(components[VERSION_COMPONENTS_TO_IDX[increment_type]]) + 1)
+    return '.'.join(components)
+
+def update_version(file_path, current_version):
+    new_version = version_increment(current_version)
+    with open(file_path, 'wb') as f:
+        f.write('{"application_version": "%s"}\n' % (new_version, ))
         
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        raise ValueError("Missing version file path parameter")
-    update_version(sys.argv[1])
+    if len(sys.argv) < 3:
+        raise ValueError("update_version.py <version file path> <current version>")
+    update_version(sys.argv[1], sys.argv[2])
