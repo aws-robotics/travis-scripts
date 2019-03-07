fdfc1557cfce7524436e439b215f717bf63a9899
AAlon <avishayalon@gmail.com>
2019-03-06 18:59:04 -0800

    Update update_version.py

--- update_version.py
+++ update_version.py
@@ -2,11 +2,20 @@ import os
 import sys
 
 VERSION_COMPONENTS_TO_IDX = {'major': 0, 'minor': 1, 'patch': 2}
+FIRST_VERSION = '1.0.0'
+FIRST_VERSION_WITH_EXISTING_OBSOLETE_TAGS = '1.1.0'
 
 def version_increment(version, increment_type='patch'):
     components = version.split('.')
-    components[VERSION_COMPONENTS_TO_IDX[increment_type]] = str(int(components[VERSION_COMPONENTS_TO_IDX[increment_type]]) + 1)
-    return '.'.join(components)
+    if len(components) != 3:
+        # Version is not bootstrapped properly. Either there was no release yet (no tags) or the format is obsolete.
+        if 'No names found' in version:
+            return FIRST_VERSION
+        else:
+            return FIRST_VERSION_WITH_EXISTING_OBSOLETE_TAGS
+    else:
+        components[VERSION_COMPONENTS_TO_IDX[increment_type]] = str(int(components[VERSION_COMPONENTS_TO_IDX[increment_type]]) + 1)
+        return '.'.join(components)
 
 def update_version(file_path, current_version):
     new_version = version_increment(current_version)
