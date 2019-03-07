9f8ca9ceb38f82d21cd3427b0b04905560e678d4
AAlon <avishayalon@gmail.com>
2019-03-06 17:21:32 -0800

    Update update_version.py

--- update_version.py
+++ update_version.py
@@ -2,4 +2,4 @@ import os
 
 if not os.path.exists('version.json'):
     with open('version.json', 'wb') as f:
-        f.write('{"application_version": "1.0.0"}')
+        f.write('{"application_version": "1.0.0"}\n')
