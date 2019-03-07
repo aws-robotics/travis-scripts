c6f83ea91cfe8b3995aaedc54927e3ba57bd8647
AAlon <avishayalon@gmail.com>
2019-03-06 22:18:53 -0800

    Update ros1_sa_build.sh

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-env
-bash -c "echo '`env | grep TRAVIS`'"
-
 # install dependencies
 apt update && apt install -y python3-pip python3-apt dpkg ros-$ROS_DISTRO-ros-base && rosdep update
 apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools
