517bcb22c70b65795710a1d5a6484bf771cc1c5b
AAlon <avishayalon@gmail.com>
2019-03-08 15:30:40 -0800

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ ! -z "${TRAVIS_TAG}" ]; then
+    # Do not run for builds triggered by tagging.
+    echo "Skipping $0"
+    exit 0
+fi
+
 # Move artifacts to shared/<version>/ and version.json to shared/
 mkdir $SA_VERSION && mv $TRAVIS_BUILD_DIR/shared/* $SA_VERSION && mv $SA_VERSION $TRAVIS_BUILD_DIR/shared/
 cp "$TRAVIS_BUILD_DIR/version.json" "$TRAVIS_BUILD_DIR/shared/version.json"
