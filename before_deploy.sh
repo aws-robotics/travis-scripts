ef998aac32e220b55f141c4f9b152148ed28252e
AAlon <avishayalon@gmail.com>
2019-03-07 21:31:42 -0800

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Move artifacts to shared/<version>/ and version.json to shared/
-mkdir $SA_VERSION && mv shared/* $SA_VERSION && mv $SA_VERSION shared/
-cp "version.json" "shared/version.json"
+mkdir $SA_VERSION && mv $TRAVIS_BUILD_DIR/shared/* $SA_VERSION && mv $SA_VERSION $TRAVIS_BUILD_DIR/shared/
+cp "$TRAVIS_BUILD_DIR/version.json" "$TRAVIS_BUILD_DIR/shared/version.json"
 # Fetch the relevant S3 bucket & CodePipeline
 export S3_BUCKET_NAME=`aws s3 ls | grep travissourcebucket | awk '{print $3}'`
 export SA_NAME_WITHOUT_DASHES=`echo $SA_NAME | sed -e 's/-//g'`
