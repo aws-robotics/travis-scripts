c4751f6294bb7cad30561674771899a64962aa47
Juan Rodriguez Hortala <hortala@amazon.com>
2019-03-07 17:13:23 -0800

    Some linting
    Add additional quotes and braces to bash scripts

--- ros1_build.sh
+++ ros1_build.sh
@@ -11,15 +11,15 @@ apt-get install python-pip -y && pip install -U coverage
 export CATKIN_TEST_COVERAGE=1
 
 # use colcon as build tool to build the package, and optionally build tests
-. /opt/ros/$ROS_DISTRO/setup.sh
-cd /"$ROS_DISTRO"_ws/
-rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -r -y
+. "/opt/ros/${ROS_DISTRO}/setup.sh"
+cd "/${ROS_DISTRO}_ws/"
+rosdep install --from-paths src --ignore-src --rosdistro "${ROS_DISTRO}" -r -y
 colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage'
 if [ -z "${NO_TEST}" ];
 then
     if [ ! -z "${PACKAGE_NAME}" ];
     then
-      colcon build --packages-select $PACKAGE_NAME --cmake-target tests
+      colcon build --packages-select "${PACKAGE_NAME}" --cmake-target tests
     fi
 
     # run unit tests
@@ -33,11 +33,11 @@ then
             lcov --capture --directory . --output-file coverage.info
             lcov --remove coverage.info '/usr/*' --output-file coverage.info
             lcov --list coverage.info
-            cd /"$ROS_DISTRO"_ws/
+            cd "/${ROS_DISTRO}_ws/"
             mv coverage.info /shared
             ;;
         "python")
-            cd /"$ROS_DISTRO"_ws/build/${PACKAGE_NAME}
+            cd "/${ROS_DISTRO}_ws/build/${PACKAGE_NAME}"
             coverage xml
             cp coverage.xml /shared/coverage.info
             ;;
