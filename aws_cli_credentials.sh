aba8ba263631e84ee9c3e71503efdaab332e3f98
AAlon <avishayalon@gmail.com>
2019-03-07 20:22:11 -0800

    Update aws_cli_credentials.sh

--- aws_cli_credentials.sh
+++ aws_cli_credentials.sh
@@ -5,5 +5,5 @@ mkdir -p ~/.aws
 cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_KEY}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
