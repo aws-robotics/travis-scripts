07e2b3f7860e75dabc7e3251d1960f378117687e
AAlon <avishayalon@gmail.com>
2019-03-08 14:46:59 -0800

    Update add_tag.sh

--- add_tag.sh
+++ add_tag.sh
@@ -19,8 +19,7 @@ git config --global user.name "${GH_USER_NAME}"
 git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
 # Commit changes to package.xml files.
 git status
-git commit *package.xml* -m "Updating package manifests for release $SA_VERSION"
-git push origin master
+env
 # Add and push tag
 git tag -a $SA_VERSION -m "Release $SA_VERSION"
 git push origin $SA_VERSION
