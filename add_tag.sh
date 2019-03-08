0235e14b67df3fb2abd7b9c37c2fe8d2ec0a0533
AAlon <avishayalon@gmail.com>
2019-03-07 21:33:27 -0800

    Update add_tag.sh

--- add_tag.sh
+++ add_tag.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Figure out what's the new version
+python $SCRIPT_DIR/update_version.py "$TRAVIS_BUILD_DIR/version.json" "`git describe --abbrev=0 --tags 2>&1`"
+export SA_VERSION=`cat $TRAVIS_BUILD_DIR/version.json | jq -r .application_version`
 # Set Travis fake user & email
 git config --global user.email "${GH_USER_EMAIL}"
 git config --global user.name "${GH_USER_NAME}"
