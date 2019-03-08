e9daf09352e33b5cfa7ceea2a26172aee8933853
AAlon <avishayalon@gmail.com>
2019-03-07 19:05:39 -0800

    Update ros1_sa_build.sh

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -25,5 +25,5 @@ colcon bundle --build-base build --install-base install --bundle-base bundle
 mv ./bundle/output.tar.gz ./bundle/simulation.tar.gz
 
 # move the artifacts to a shared mount point
-mv /"$ROS_DISTRO"_ws/src/$BUILD_DIR_NAME/robot_ws/bundle/robot.tar.gz /shared/robot.tar.gz
-mv /"$ROS_DISTRO"_ws/src/$BUILD_DIR_NAME/simulation_ws/bundle/simulation.tar.gz /shared/simulation.tar.gz
+mv /"$ROS_DISTRO"_ws/src/$BUILD_DIR_NAME/robot_ws/bundle/robot.tar.gz /shared/robot_ws.tar.gz
+mv /"$ROS_DISTRO"_ws/src/$BUILD_DIR_NAME/simulation_ws/bundle/simulation.tar.gz /shared/simulation_ws.tar.gz
