dc184a44c77dfbb87b78223ca13218e941988777
AAlon <avishayalon@gmail.com>
2019-03-08 15:37:38 -0800

    Update add_tag.sh

--- add_tag.sh
+++ add_tag.sh
@@ -23,8 +23,6 @@ git config --global user.email "${GH_USER_EMAIL}"
 git config --global user.name "${GH_USER_NAME}"
 # Switch to SSH url
 git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
-# Commit changes to package.xml files.
-echo `env`
 # Add and push tag
 git tag -a $SA_VERSION -m "Release $SA_VERSION"
 git push origin $SA_VERSION
