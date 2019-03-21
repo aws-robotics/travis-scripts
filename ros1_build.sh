a461291a05daedbf560743357cad13f6b2a52841
Ryan Newell <ryanewel@amazon.com>
2019-03-21 16:44:36 -0700

    rename build files

--- ros1_build.sh
+++ ros1_build.sh
@@ -1,45 +1,54 @@
 #!/bin/bash
 set -e
 
-# install dependencies
-apt update && apt install -y lcov python3-pip libgtest-dev cmake && rosdep update
-cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib
-apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools
-# nosetests needs coverage for Python 2
-apt-get install python-pip -y && pip install -U coverage
-# enable Python coverage "https://github.com/ros/catkin/blob/kinetic-devel/cmake/test/nosetests.cmake#L59"
-export CATKIN_TEST_COVERAGE=1
+if [ ! -z "${TRAVIS_TAG}" ]; then
+    # Do not run for builds triggered by tagging.
+    echo "Skipping $0 $@"
+    exit 0
+fi
 
-# use colcon as build tool to build the package, and optionally build tests
-. "/opt/ros/${ROS_DISTRO}/setup.sh"
-cd "/${ROS_DISTRO}_ws/"
-rosdep install --from-paths src --ignore-src --rosdistro "${ROS_DISTRO}" -r -y
-colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage'
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# create a shared mount point to put coverage report
+mkdir shared/
+# move travis scripts to share mount so they can be executed within the docker container
+cp -r "${SCRIPT_DIR}" shared/
+if [ -z "${SA_NAME}" ];
+then
+  # SA_NAME not set - assume a Cloud Extension build
+  BUILD_SCRIPT_NAME=ros"$ROS_VERSION"_ce_build.sh
+else
+  # SA_NAME is set - assume a Sample Application build
+  BUILD_SCRIPT_NAME=ros"$ROS_VERSION"_sa_build.sh
+fi
+
+echo "using Build script, ${BUILD_SCRIPT_NAME}"
+DOCKER_BUILD_SCRIPT="/shared/$(basename ${SCRIPT_DIR})/${BUILD_SCRIPT_NAME}"
+# get a docker container from OSRF's docker hub
+docker pull "ros:${ROS_DISTRO}-ros-core"
+# run docker container
+docker run -v "${PWD}/shared:/shared" \
+  -e ROS_DISTRO="${ROS_DISTRO}" \
+  -e PACKAGE_NAME="${PACKAGE_NAME}" \
+  -e ROS_VERSION="${ROS_VERSION}" \
+  -e NO_TEST="${NO_TEST}" \
+  -e TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR}" \
+  -e PACKAGE_LANG="${PACKAGE_LANG:-cpp}" \
+  -e GAZEBO_VERSION="${GAZEBO_VERSION:-7}" \
+  -e DOCKER_BUILD_SCRIPT="${DOCKER_BUILD_SCRIPT}" \
+  -e WORKSPACES="${WORKSPACES}" \
+  --name "${ROS_DISTRO}-container" \
+  -dit "ros:${ROS_DISTRO}-ros-core" /bin/bash
+# make a workspace in the docker container
+docker exec "${ROS_DISTRO}-container" /bin/bash -c 'mkdir -p "/${ROS_DISTRO}_ws/src"'
+# copy the code over to the docker container
+docker cp "${TRAVIS_BUILD_DIR}" "${ROS_DISTRO}-container":"/${ROS_DISTRO}_ws/src/"
+# execute build scripts and run test
+
+docker exec "${ROS_DISTRO}"-container /bin/bash \
+  -c "sh ${DOCKER_BUILD_SCRIPT}"
+# upload coverage report to codecov
 if [ -z "${NO_TEST}" ];
 then
-    if [ ! -z "${PACKAGE_NAME}" ];
-    then
-      colcon build --packages-select "${PACKAGE_NAME}" --cmake-target tests
-    fi
-
-    # run unit tests
-    . ./install/setup.sh
-    colcon test
-    colcon test-result --all
-
-    # get unit test code coverage result
-    case ${PACKAGE_LANG} in 
-        "cpp") 
-            lcov --capture --directory . --output-file coverage.info
-            lcov --remove coverage.info '/usr/*' --output-file coverage.info
-            lcov --list coverage.info
-            cd "/${ROS_DISTRO}_ws/"
-            mv coverage.info /shared
-            ;;
-        "python")
-            cd "/${ROS_DISTRO}_ws/build/${PACKAGE_NAME}"
-            coverage xml
-            cp coverage.xml /shared/coverage.info
-            ;;
-    esac
+  bash <(curl -s https://codecov.io/bash) -Z -F "${ROS_DISTRO},ROS_${ROS_VERSION}"
 fi
---
+++ ros1_ce_build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+# install dependencies
+apt update && apt install -y lcov python3-pip libgtest-dev cmake && rosdep update
+cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib
+apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools
+# nosetests needs coverage for Python 2
+apt-get install python-pip -y && pip install -U coverage
+# enable Python coverage "https://github.com/ros/catkin/blob/kinetic-devel/cmake/test/nosetests.cmake#L59"
+export CATKIN_TEST_COVERAGE=1
+
+# use colcon as build tool to build the package, and optionally build tests
+. "/opt/ros/${ROS_DISTRO}/setup.sh"
+cd "/${ROS_DISTRO}_ws/"
+rosdep install --from-paths src --ignore-src --rosdistro "${ROS_DISTRO}" -r -y
+colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage'
+if [ -z "${NO_TEST}" ];
+then
+    if [ ! -z "${PACKAGE_NAME}" ];
+    then
+      colcon build --packages-select "${PACKAGE_NAME}" --cmake-target tests
+    fi
+
+    # run unit tests
+    . ./install/setup.sh
+    colcon test
+    colcon test-result --all
+
+    # get unit test code coverage result
+    case ${PACKAGE_LANG} in 
+        "cpp") 
+            lcov --capture --directory . --output-file coverage.info
+            lcov --remove coverage.info '/usr/*' --output-file coverage.info
+            lcov --list coverage.info
+            cd "/${ROS_DISTRO}_ws/"
+            mv coverage.info /shared
+            ;;
+        "python")
+            cd "/${ROS_DISTRO}_ws/build/${PACKAGE_NAME}"
+            coverage xml
+            cp coverage.xml /shared/coverage.info
+            ;;
+    esac
+fi
