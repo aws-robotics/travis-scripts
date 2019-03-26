b43139cf0f69a0046e7532536c4e8028d7cf7c7b
Abby Xu <abbyxu@amazon.com>
2019-03-26 11:23:32 -0700

    test version.json file upload to cc

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 set -e
-
-if [ ! -z "${TRAVIS_TAG}" ]; then
-    # Do not run for builds triggered by tagging.
-    echo "Skipping $0 $@"
-    exit 0
-fi
-
-# Move artifacts to shared/<version>/ and version.json to shared/
-mkdir $SA_VERSION && mv $TRAVIS_BUILD_DIR/shared/* $SA_VERSION && mv $SA_VERSION $TRAVIS_BUILD_DIR/shared/
-cp "$TRAVIS_BUILD_DIR/version.json" "$TRAVIS_BUILD_DIR/shared/version.json"
-# Fetch the relevant S3 bucket & CodePipeline
-export S3_BUCKET_NAME=`aws s3 ls | grep travissourcebucket | awk '{print $3}'`
-export SA_NAME_WITHOUT_DASHES=`echo $SA_NAME | sed -e 's/-//g'`
-export CODE_PIPELINE_NAME=`aws codepipeline list-pipelines | jq -r '.pipelines | .[] | .name' | grep "${SA_NAME_WITHOUT_DASHES}pipeline"`
 export MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json | jq -r '.commitId'`
--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -1,47 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ ! -z "${TRAVIS_TAG}" ]; then
-    # Do not run for builds triggered by tagging.
-    echo "Skipping $0 $@"
-    exit 0
-fi
-
-if [ -z "${PIPELINE_TIMEOUT}" ]; then
-    # 30 minutes by default
-    PIPELINE_TIMEOUT=1800
-fi
-
 # Upload version.json to CodeCommit
 if [ -z "${MASTER_COMMIT_ID}" ]; then
     # version.json file being uploaded the first time
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$TRAVIS_BUILD_DIR/shared/version.json" --file-path version.json --file-mode normal
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$TRAVIS_BUILD_DIR/shared/version.json" --file-path version.json --file-mode normal --commit-message $TRAVIS_COMMIT_MESSAGE --name $GH_USER_NAME --email $GH_USER_EMAIL
 else
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$TRAVIS_BUILD_DIR/shared/version.json" --file-path version.json --file-mode normal --parent-commit-id "$MASTER_COMMIT_ID"
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$TRAVIS_BUILD_DIR/shared/version.json" --file-path version.json --file-mode normal --commit-message $TRAVIS_COMMIT_MESSAGE --name $GH_USER_NAME --email $GH_USER_EMAIL --parent-commit-id "$MASTER_COMMIT_ID"
 fi
 
-# Start execution
-PIPELINE_EXECUTION_ID=`aws codepipeline start-pipeline-execution --name "$CODE_PIPELINE_NAME" | jq -r .pipelineExecutionId`
 
-starting_time=`date +%s`
-retry_interval=5 # Poll every 5 seconds
-elapsed_time=0
-
-while [ $elapsed_time -lt $PIPELINE_TIMEOUT ]; do 
-  status=`aws codepipeline get-pipeline-execution --pipeline-name "$CODE_PIPELINE_NAME" --pipeline-execution-id "$PIPELINE_EXECUTION_ID" | jq -r .pipelineExecution.status`
-  echo "Pipeline status: $status"
-  # status corresponds to https://docs.aws.amazon.com/codepipeline/latest/APIReference/API_PipelineExecution.html#CodePipeline-Type-PipelineExecution-status
-  if [ "$status" = "Succeeded" ] || [ "$status" = "Superseded" ]; then
-    echo "Pipeline execution finished."
-    exit 0
-  elif [ "$status" = "Failed" ]; then
-    echo "Pipeline execution failed."
-    exit 1
-  else
-    sleep $retry_interval
-    let elapsed_time="`date +%s` - $starting_time"
-  fi
-done
-
-echo "Reached timeout waiting for pipeline to finish."
-exit 1
