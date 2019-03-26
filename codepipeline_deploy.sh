38338346520ce9458a2267fa01ee004e099f39f2
Abby Xu <abbyxu@amazon.com>
2019-03-26 13:48:31 -0700

    test

--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -8,7 +8,7 @@ if [ -z "${MASTER_COMMIT_ID}" ]; then
     # version.json file being uploaded the first time
     aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json" --file-path version.json --file-mode normal
 else
-aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json" --file-path version.json --file-mode normal --commit-message $TRAVIS_COMMIT_MESSAGE --name $GH_USER_NAME --email $GH_USER_EMAIL --parent-commit-id "$MASTER_COMMIT_ID"
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json" --file-path version.json --file-mode normal --commit-message $TRAVIS_COMMIT_MESSAGE --name $GH_USER_NAME --email $GH_USER_EMAIL --parent-commit-id "$MASTER_COMMIT_ID"
 fi
 
 
