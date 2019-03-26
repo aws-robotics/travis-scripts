1d61f7c90eade4925106a12ecde7af26ff17d693
Abby Xu <abbyxu@amazon.com>
2019-03-26 12:58:28 -0700

    test exit code

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json` || exit 0
-export MASTER_COMMIT_ID=`echo $MASTER_COMMIT_ID | jq -r '.commitId'`
+if MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json` || exit 0; then
+    export MASTER_COMMIT_ID=`echo $MASTER_COMMIT_ID | jq -r '.commitId'`
+fi
