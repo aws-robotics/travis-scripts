b5c1ecd86d50907e83d504e428c1effd0f88fb2d
Abby Xu <abbyxu@amazon.com>
2019-03-26 13:41:51 -0700

    added test version

--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
+touch version.json
+echo testtest1 >> version.json
 
-echo "$TRAVIS_BUILD_DIR/shared/version.json"
-echo "${CC_REPO_NAME}"
 # Upload version.json to CodeCommit
 if [ -z "${MASTER_COMMIT_ID}" ]; then
     # version.json file being uploaded the first time
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$TRAVIS_BUILD_DIR/version.json" --file-path version.json --file-mode normal
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$pwd/version.json" --file-path version.json --file-mode normal
 else
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$TRAVIS_BUILD_DIR/version.json" --file-path version.json --file-mode normal
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$pwd/version.json" --file-path version.json --file-mode normal
 fi
 
 
