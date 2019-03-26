dc76f0e98f4002ce97110190e00206285930e008
Abby Xu <abbyxu@amazon.com>
2019-03-26 12:49:36 -0700

    test error code

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+set -e
 
-export MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json | jq -r '.commitId'`
-if [ $? -eq 255 ]; then
-    echo version.json file does not yet exist in CodeCommit repository named "$CC_REPO_NAME"
-    exit 0
-fi
+MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json` || exit 0
+export MASTER_COMMIT_ID=`echo $MASTER_COMMIT_ID | jq -r '.commitId'`
