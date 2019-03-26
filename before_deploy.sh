e5e958065a0e4fc1265aaffc063e202a0adf808d
Abby Xu <abbyxu@amazon.com>
2019-03-26 14:07:23 -0700

    test

--- before_deploy.sh
+++ before_deploy.sh
@@ -6,8 +6,10 @@ echo testtest1 >> version.json
 # Move artifacts to shared/<version>/ and version.json to shared/
 
 MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json`
+echo $MASTER_COMMIT_ID
 if [ $? -ne 0 ]; then
     echo test
 else
     export MASTER_COMMIT_ID=`echo $MASTER_COMMIT_ID | jq -r '.commitId'`
+    echo $MASTER_COMMIT_ID
 fi
