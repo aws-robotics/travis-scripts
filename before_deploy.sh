032f3cc7088657c32e9e90b95fdd7b214dc51408
Abby Xu <abbyxu@amazon.com>
2019-03-26 12:18:05 -0700

    test exit code

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,3 +2,7 @@
 set -e
 
 export MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json | jq -r '.commitId'`
+if [ "$?" -eq "255" ]; then
+    echo version.json file does not yet exist in CodeCommit repository named "$CC_REPO_NAME"
+    exit 0
+fi
