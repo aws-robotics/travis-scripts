a94121275ed88cb6058a02a434ce80f8366e5e2b
Abby Xu <abbyxu@amazon.com>
2019-03-26 13:05:20 -0700

    test exit code

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json`
+MASTER_COMMIT_ID=`aws codecommit get-file --repository-name test1 --file-path test1`
 if [ $? -ne 0 ]; then
     echo test
 else
