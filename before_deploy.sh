c06fbc236796b1a239a948c3f2a74b3c7d6545f9
Abby Xu <abbyxu@amazon.com>
2019-03-26 14:18:26 -0700

    test

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 export SA_VERSION=1
-touch version.json
-echo testtest1 >> version.json
-
-# Move artifacts to shared/<version>/ and version.json to shared/
-
-MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json`
-echo $MASTER_COMMIT_ID
-if [ $? -ne 0 ]; then
-    echo test
-    exit 0
-fi
-export MASTER_COMMIT_ID=`echo $MASTER_COMMIT_ID | jq -r '.commitId'`
-echo $MASTER_COMMIT_ID
 
--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -3,9 +3,22 @@ set -e
 touch version.json
 echo testtest1 >> version.json
 
+touch version.json
+echo testtest1 >> version.json
+
+# Move artifacts to shared/<version>/ and version.json to shared/
+
+MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json`
 echo $MASTER_COMMIT_ID
+if [ $? -ne 0 ]; then
+    echo test
+    exit 0
+fi
+export MASTER_COMMIT_ID=`echo $MASTER_COMMIT_ID | jq -r '.commitId'`
+echo $MASTER_COMMIT_ID
+
 # Upload version.json to CodeCommit
-if [ ! -z "${MASTER_COMMIT_ID}" ]; then
+if [ -z "${MASTER_COMMIT_ID}" ]; then
     echo test
     # version.json file being uploaded the first time
     aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json --file-path version.json --file-mode normal --commit-message "$TRAVIS_COMMIT_MESSAGE" --name "$GH_USER_NAME" --email "$GH_USER_EMAIL"
