adcea7791d7622e4bfed855cb1de769afce42d1e
Abby Xu <abbyxu@amazon.com>
2019-03-26 13:17:19 -0700

    run script

--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "$TRAVIS_BUILD_DIR/shared/version.json"
+echo "${CC_REPO_NAME}"
 # Upload version.json to CodeCommit
 if [ -z "${MASTER_COMMIT_ID}" ]; then
     # version.json file being uploaded the first time
