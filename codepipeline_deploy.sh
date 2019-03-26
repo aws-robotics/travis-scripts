f2e63ef84bf5621a0a7a53fe1d687392c4f32eef
Abby Xu <abbyxu@amazon.com>
2019-03-26 14:01:00 -0700

    test

--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -6,9 +6,9 @@ echo testtest1 >> version.json
 # Upload version.json to CodeCommit
 if [ -z "${MASTER_COMMIT_ID}" ]; then
     # version.json file being uploaded the first time
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json" --file-path version.json --file-mode normal --commit-message $TRAVIS_COMMIT_MESSAGE --name $GH_USER_NAME --email $GH_USER_EMAIL
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json --file-path version.json --file-mode normal --commit-message $TRAVIS_COMMIT_MESSAGE --name $GH_USER_NAME --email $GH_USER_EMAIL
 else
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json" --file-path version.json --file-mode normal --commit-message $TRAVIS_COMMIT_MESSAGE --name $GH_USER_NAME --email $GH_USER_EMAIL --parent-commit-id "$MASTER_COMMIT_ID"
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json --file-path version.json --file-mode normal --commit-message $TRAVIS_COMMIT_MESSAGE --name $GH_USER_NAME --email $GH_USER_EMAIL --parent-commit-id "$MASTER_COMMIT_ID"
 fi
 
 
