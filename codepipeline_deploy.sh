4fb049e6d44c4b4847e3dbe588ca478565080685
Abby Xu <abbyxu@amazon.com>
2019-03-26 13:44:01 -0700

    added test version

--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -6,9 +6,9 @@ echo testtest1 >> version.json
 # Upload version.json to CodeCommit
 if [ -z "${MASTER_COMMIT_ID}" ]; then
     # version.json file being uploaded the first time
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$pwd/version.json" --file-path version.json --file-mode normal
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json --file-path version.json --file-mode normal
 else
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://"$pwd/version.json" --file-path version.json --file-mode normal
+    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json --file-path version.json --file-mode normal
 fi
 
 
