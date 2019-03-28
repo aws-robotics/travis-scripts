c59ab440afb07a8ceeb6ca38a6d5e0f204edcc08
Ryan Newell <ryanewel@amazon.com>
2019-03-28 13:11:09 -0700

    set default WS, fail when not found

--- ws_builds/robot_ws.sh
+++ ws_builds/robot_ws.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 rosws update
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base build --install-base install
-colcon bundle --build-base build --install-base install --bundle-base bundle
+colcon bundle --bundle-version 1 --build-base build --install-base install --bundle-base bundle
