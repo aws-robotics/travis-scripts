3f1231cf563edaaf3b3fbe21f19a7177c98f6017
Abby Xu <abbyxu@amazon.com>
2019-03-29 13:22:50 -0700

    test

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export SA_VERSION=1
+-e
 
+export SA_VERSION=1
+export CC_REPO_CLONE_URL_HTTP=`aws codecommit get-repository --repository-name AppManifest-"$SA_NAME"-"$ROS_DISTRO"-gazebo"$GAZEBO_VERSION" | jq -r '.repositoryMetadata | .cloneUrlHttp'`
--- codepipeline_deploy.sh
+++ codepipeline_deploy.sh
@@ -6,26 +6,11 @@ echo testtest1 >> version.json
 touch version.json
 echo testtest1 >> version.json
 
-# Move artifacts to shared/<version>/ and version.json to shared/
 
-MASTER_COMMIT_ID=`aws codecommit get-file --repository-name "${CC_REPO_NAME}" --file-path version.json`
-echo $MASTER_COMMIT_ID
-if [ $? -ne 0 ]; then
-    echo test
-    exit 0
-fi
-export MASTER_COMMIT_ID=`echo $MASTER_COMMIT_ID | jq -r '.commitId'`
-echo $MASTER_COMMIT_ID
-
-# Upload version.json to CodeCommit
-if [ -z "${MASTER_COMMIT_ID}" ]; then
-    echo test
-    # version.json file being uploaded the first time
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json --file-path version.json --file-mode normal --commit-message "$TRAVIS_COMMIT_MESSAGE" --name "$GH_USER_NAME" --email "$GH_USER_EMAIL"
-else
-    echo here
-    aws codecommit put-file --repository-name "${CC_REPO_NAME}" --branch-name master --file-content file://./version.json --file-path version.json --file-mode normal --commit-message "$TRAVIS_COMMIT_MESSAGE" --name "$GH_USER_NAME" --email "$GH_USER_EMAIL" --parent-commit-id "$MASTER_COMMIT_ID"
-
-fi
-
-
+# Clone the CC repository where version.json lives and commit the version.json file produced by current build
+git clone "$CC_REPO_CLONE_URL_HTTP" cc-repo-for-version-file
+cd cc-repo-for-version-file
+cp "$TRAVIS_BUILD_DIR/shared/version.json" version.json
+git add version.json
+git commit -m "updating version.json file by commitId: $MASTER_COMMIT_ID"
+git push
