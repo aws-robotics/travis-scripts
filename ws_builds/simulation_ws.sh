7366bcd646bd17cab6de7b9f12965f0dbfc2171d
AAlon <avishayalon@gmail.com>
2019-04-04 14:34:31 -0700

    Minor changes for compatibility with v2 colcon bundles (#18)
    * Remove --bundle-version 1 from simulation_ws.sh
    
    * Remove --bundle-version 1 from robot_ws.sh
    
    * Rename .tar.gz to .tar in ros1_sa_build.sh

--- ws_builds/simulation_ws.sh
+++ ws_builds/simulation_ws.sh
@@ -4,4 +4,4 @@ rosdep install --from-paths src --ignore-src -r -y
 GAZEBO_POST_ROSDEP_INSTALL_SCRIPT="${SCRIPT_DIR}/gazebo/${GAZEBO_VERSION}/post_rosdep_install.sh"
 if [ -f ${GAZEBO_POST_ROSDEP_INSTALL_SCRIPT} ]; then bash ${GAZEBO_POST_ROSDEP_INSTALL_SCRIPT}; fi
 colcon build --build-base build --install-base install
-colcon bundle --bundle-version 1 --build-base build --install-base install --bundle-base bundle
+colcon bundle --build-base build --install-base install --bundle-base bundle
