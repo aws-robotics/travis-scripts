2c589ea9076814916ae929344eadf628d568a6b2
AAlon <avishayalon@gmail.com>
2019-04-04 13:55:14 -0700

    Remove --bundle-version 1 from robot_ws.sh

--- ws_builds/robot_ws.sh
+++ ws_builds/robot_ws.sh
@@ -2,4 +2,4 @@
 rosws update
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base build --install-base install
-colcon bundle --bundle-version 1 --build-base build --install-base install --bundle-base bundle
+colcon bundle --build-base build --install-base install --bundle-base bundle
