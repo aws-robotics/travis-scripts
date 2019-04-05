5b56e0662f42c76ff4c5b92921f9f6b307df7ba6
AAlon <avishayalon@gmail.com>
2019-04-04 22:47:54 -0700

    Look for the correct pipeline name in before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -13,4 +13,4 @@ cp "$TRAVIS_BUILD_DIR/version.json" "$TRAVIS_BUILD_DIR/shared/version.json"
 # Fetch the relevant S3 bucket & CodePipeline
 export S3_BUCKET_NAME=`aws s3 ls | grep travissourcebucket | awk '{print $3}'`
 export SA_NAME_WITHOUT_DASHES=`echo $SA_NAME | sed -e 's/-//g'`
-export CODE_PIPELINE_NAME=`aws codepipeline list-pipelines | jq -r '.pipelines | .[] | .name' | grep "${SA_NAME_WITHOUT_DASHES}pipeline"`
+export CODE_PIPELINE_NAME=`aws codepipeline list-pipelines | jq -r '.pipelines | .[] | .name' | grep "${SA_NAME_WITHOUT_DASHES}" | grep "gazebo${GAZEBO_VERSION}"`
