03559a4b7398c2a8da1aa640aef45eb9920c2ddb
Tim Robinson <tim@amazon.com>
2019-04-17 13:00:51 -0700

    Run rosdep update and install after switching to gazebo9

--- ws_builds/simulation_ws.sh
+++ ws_builds/simulation_ws.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -ex
 rosws update
-rosdep install --from-paths src --ignore-src -r -y
 GAZEBO_POST_ROSDEP_INSTALL_SCRIPT="${SCRIPT_DIR}/gazebo/${GAZEBO_VERSION}/post_rosdep_install.sh"
 if [ -f ${GAZEBO_POST_ROSDEP_INSTALL_SCRIPT} ]; then bash ${GAZEBO_POST_ROSDEP_INSTALL_SCRIPT}; fi
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base build --install-base install
 colcon bundle --build-base build --install-base install --bundle-base bundle
