392c5542a84630582e1d9ef5b0d1545f55f05503
Abby Xu <abbyxu@amazon.com>
2019-04-18 10:17:24 -0700

    skip deployment related steps if travis is triggered by cron job

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ "${TRAVIS_EVENT_TYPE}" == "cron"]; then
+    # Do not run for builds triggered by cron jobs.
+    echo "Skipping cron-job-triggered builds"
+    exit 0
+fi
+
 if [ ! -z "${TRAVIS_TAG}" ]; then
     # Do not run for builds triggered by tagging.
     echo "Skipping $0 $@"
