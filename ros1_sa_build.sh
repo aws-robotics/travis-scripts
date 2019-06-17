a1570057c8c4359722552b808c1d8f9cb834d216
AAlon <avishaya@amazon.com>
2019-06-17 16:21:31 -0700

    Always install colcon bundle from source

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -10,6 +10,13 @@ sh -c 'echo "deb  http://13.52.195.14/ubuntu/main $(lsb_release -sc) main" > /et
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt-get update && apt-get install --no-install-recommends -y python-rosdep python-rosinstall python3-colcon-common-extensions ros-$ROS_DISTRO-ros-base
 pip3 install colcon-bundle colcon-ros-bundle
+# Get latest colcon bundle
+COLCON_BUNDLE_INSTALL_PATH="${HOME}/colcon-bundle"
+rm -rf "${COLCON_BUNDLE_INSTALL_PATH}"
+git clone https://github.com/colcon/colcon-bundle "${COLCON_BUNDLE_INSTALL_PATH}"
+pip3 install --upgrade pip
+pip install -U --editable "${COLCON_BUNDLE_INSTALL_PATH}"
+
 # Remove the old rosdep sources.list
 rm -rf /etc/ros/rosdep/sources.list.d/*
 rosdep init && rosdep update
