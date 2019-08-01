709e9f9051a8ce101236f5d1603edf36dc2eb25d
AAlon <avishayalon@gmail.com>
2019-08-01 16:56:59 -0700

    Run post-rosdep step for robot_ws as well

--- ws_builds/robot_ws.sh
+++ ws_builds/robot_ws.sh
@@ -2,6 +2,11 @@
 set -xe
 
 rosws update
+GAZEBO_POST_ROSDEP_INSTALL_SCRIPT="${SCRIPT_DIR}/gazebo/${ROS_DISTRO}-gazebo${GAZEBO_VERSION}/post_rosdep_install.sh"
+if [ -f ${GAZEBO_POST_ROSDEP_INSTALL_SCRIPT} ]; then
+    "${GAZEBO_POST_ROSDEP_INSTALL_SCRIPT}"
+fi
+
 rosdep install --from-paths src --ignore-src -r -y
 
 colcon build --build-base build --install-base install
