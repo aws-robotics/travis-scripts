6a136396d01276f239008f1e3c0d413d7826e9f4
AAlon <avishaya@amazon.com>
2019-08-01 00:49:29 -0700

    Update ros2_sa_build.sh

--- ros2_sa_build.sh
+++ ros2_sa_build.sh
@@ -18,7 +18,7 @@ rm -rf "${COLCON_BUNDLE_INSTALL_PATH}"
 git clone https://github.com/colcon/colcon-bundle "${COLCON_BUNDLE_INSTALL_PATH}"
 
 pip3 install --upgrade pip
-pip3 install -U --editable "${COLCON_BUNDLE_INSTALL_PATH}"
+pip install -U --editable "${COLCON_BUNDLE_INSTALL_PATH}"
 
 # Remove the old rosdep sources.list
 rm -rf /etc/ros/rosdep/sources.list.d/*
