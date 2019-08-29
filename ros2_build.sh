1121dcddcd50c7394470a733160de4e9b6a4a8ed
AAlon <avishaya@amazon.com>
2019-08-29 16:40:47 -0700

    Install coverage via pip3

--- ros2_build.sh
+++ ros2_build.sh
@@ -5,7 +5,7 @@ set -xe
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
 echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list
 apt update && apt install -y python3 python3-pip lcov cmake && rosdep update
-apt update && apt install -y python3-rosinstall python3-colcon-common-extensions && pip3 install -U setuptools
+apt update && apt install -y python3-rosinstall python3-colcon-common-extensions && pip3 install -U setuptools coverage
 # Manually install python3-boto3 (temporary measure until this dependency is available via rosdep).
 apt install -y python3-boto3
 apt list --upgradable 2>/dev/null | awk {'print $1'} | sed 's/\/.*//g' | grep $ROS_DISTRO | xargs apt install -y
