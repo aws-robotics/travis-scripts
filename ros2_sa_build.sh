f8f6d3c5a1f7b43bea201cddd80017caa3dec445
AAlon <avishaya@amazon.com>
2019-09-06 12:07:54 -0700

    Use ros2-testing apt repo for ROS2 SA builds

--- ros2_sa_build.sh
+++ ros2_sa_build.sh
@@ -8,7 +8,7 @@ apt-get update && apt-get install -q -y dirmngr curl gnupg2 lsb-release zip pyth
 pip3 install -U setuptools
 
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
-sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2-testing/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 apt-get update && apt-get install --no-install-recommends -y python3-rosdep python3-rosinstall python3-colcon-common-extensions ros-$ROS_DISTRO-ros-base
 apt list --upgradable 2>/dev/null | awk {'print $1'} | sed 's/\/.*//g' | grep $ROS_DISTRO | xargs apt install -y
 pip3 install colcon-bundle colcon-ros-bundle
