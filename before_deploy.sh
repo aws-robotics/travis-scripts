67d68b40bc5c7804caa828da62e405d21069f052
AAlon <avishaya@amazon.com>
2019-10-01 11:25:09 -0700

    Restore exit on error setting in before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -14,3 +14,6 @@ cp "$TRAVIS_BUILD_DIR/version.json" "$TRAVIS_BUILD_DIR/shared/version.json"
 export S3_BUCKET_NAME=`aws s3 ls | grep "travis-source" | awk '{print $3}'`
 export SA_NAME_WITHOUT_DASHES=`echo $SA_NAME | sed -e 's/-//g'`
 export APP_MANIFEST_REPO="AppManifest-$SA_NAME-$ROS_DISTRO-gazebo$GAZEBO_VERSION"
+
+# Restore default setting to be able to safely source this script in Travis: https://github.com/travis-ci/travis-ci/issues/891
+set +e
