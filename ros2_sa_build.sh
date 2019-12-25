a8137fcaacdfc42f7abea7756d636903ff146d6f
Miaofei <miaofei@amazon.com>
2019-12-24 16:27:56 -0800

    build ros2/gazebo9 sample apps as non-root

--- ros2_sa_build.sh
+++ ros2_sa_build.sh
@@ -6,21 +6,21 @@ export SCRIPT_DIR=$(dirname ${DOCKER_BUILD_SCRIPT})
 # install dependencies
 ROS_BOOTSTRAP_SCRIPT=${SCRIPT_DIR}/ros_bootstrap.sh
 "${ROS_BOOTSTRAP_SCRIPT}"
-apt-get update && apt-get install --no-install-recommends -y python3-colcon-common-extensions ros-$ROS_DISTRO-ros-base
-apt list --upgradable 2>/dev/null | awk {'print $1'} | sed 's/\/.*//g' | grep $ROS_DISTRO | xargs apt install -y
-pip3 install colcon-bundle colcon-ros-bundle
+sudo apt-get update && sudo apt-get install --no-install-recommends -y python3-colcon-common-extensions ros-$ROS_DISTRO-ros-base
+apt list --upgradable 2>/dev/null | awk {'print $1'} | sed 's/\/.*//g' | grep $ROS_DISTRO | xargs sudo apt-get install -y
+sudo -H pip3 install -U colcon-bundle colcon-ros-bundle
 
 # Get latest colcon bundle
 COLCON_BUNDLE_INSTALL_PATH="${HOME}/colcon-bundle"
-rm -rf "${COLCON_BUNDLE_INSTALL_PATH}"
+sudo rm -rf "${COLCON_BUNDLE_INSTALL_PATH}"
 COLCON_ROS_BUNDLE_INSTALL_PATH="${HOME}/colcon-ros-bundle"
-rm -rf "${COLCON_ROS_BUNDLE_INSTALL_PATH}"
+sudo rm -rf "${COLCON_ROS_BUNDLE_INSTALL_PATH}"
 git clone https://github.com/colcon/colcon-bundle "${COLCON_BUNDLE_INSTALL_PATH}"
 git clone https://github.com/colcon/colcon-ros-bundle "${COLCON_ROS_BUNDLE_INSTALL_PATH}"
 
-pip3 install --upgrade pip
-pip install -U --editable "${COLCON_BUNDLE_INSTALL_PATH}"
-pip install -U --editable "${COLCON_ROS_BUNDLE_INSTALL_PATH}"
+sudo -H pip3 install -U pip
+sudo -H pip install -U --editable "${COLCON_BUNDLE_INSTALL_PATH}"
+sudo -H pip install -U --editable "${COLCON_ROS_BUNDLE_INSTALL_PATH}"
 
 COMMON_SA_BUILD_SCRIPT=${SCRIPT_DIR}/common_sa_build.sh
 . "${COMMON_SA_BUILD_SCRIPT}"
