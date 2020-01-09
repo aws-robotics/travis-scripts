d508f52258d6d85d9443c194aa6b0d1382d39aa6
Avishay Alon <avishaya@amazon.com>
2020-01-08 16:09:24 -0800

    Install colcon-core 0.4.5 in ros1_sa_build.sh

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -7,6 +7,7 @@ export SCRIPT_DIR=$(dirname ${DOCKER_BUILD_SCRIPT})
 ROS_BOOTSTRAP_SCRIPT=${SCRIPT_DIR}/ros_bootstrap.sh
 "${ROS_BOOTSTRAP_SCRIPT}"
 sudo apt-get update && sudo apt-get install --no-install-recommends -y python3-colcon-common-extensions ros-${ROS_DISTRO}-ros-base
+sudo -H pip3 install -U colcon-core==0.4.5
 sudo -H pip3 install -U colcon-bundle colcon-ros-bundle
 
 COMMON_SA_BUILD_SCRIPT=${SCRIPT_DIR}/common_sa_build.sh
