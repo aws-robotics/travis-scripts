6ceae3c05cd86457551ddbb4042d4e8708c04a4e
Anson Wong <wongtsan@amazon.com>
2020-09-01 11:27:37 -0700

    Remove comment

--- ros_bootstrap.sh
+++ ros_bootstrap.sh
@@ -3,7 +3,6 @@ set -xe
 
 # Set up ROS APT and install basic dependencies (rosdep, rosinstall). Must have ROS_VERSION set when called.
 sudo apt-get update && sudo apt-get install -q -y dirmngr curl gnupg2 lsb-release zip python3-pip python3-apt dpkg
-# sudo -H pip3 install -U setuptools!=50.0.0
 
 if [ "${ROS_VERSION}" == "1" ]; then
   echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros-latest.list
