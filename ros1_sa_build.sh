1414f0f8ae20b2722ad1459eb77ffae73b6b60fd
Anson Wong <wongtsan@amazon.com>
2020-09-01 13:48:27 -0700

    Pin 49.3.0 for setuptools

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -9,9 +9,8 @@ ROS_BOOTSTRAP_SCRIPT=${SCRIPT_DIR}/ros_bootstrap.sh
 sudo apt-get update && sudo apt-get install --no-install-recommends -y python3-colcon-common-extensions ros-${ROS_DISTRO}-ros-base
 sudo -H pip3 install -U colcon-bundle colcon-ros-bundle
 # colcon-bundle setup requirements reinstalls the latest version(50.0.0) of setuptools.
-sudo -H pip3 install -U setuptools!=50.0.0 --force-reinstall
-# temp workaround for setuptools 50.0.0 issue
-export SETUPTOOLS_USE_DISTUTILS=stdlib
+# pin the latest tested version (49.3.0)
+sudo -H pip3 install -U setuptools==49.3.0 --force-reinstall
 
 COMMON_SA_BUILD_SCRIPT=${SCRIPT_DIR}/common_sa_build.sh
 . "${COMMON_SA_BUILD_SCRIPT}"
