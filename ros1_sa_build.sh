952dbd9d21e48c3683d310cb6f1292832c97193f
Anson Wong <wongtsan@amazon.com>
2020-09-01 13:11:55 -0700

    Use recommended workaround

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -10,6 +10,8 @@ sudo apt-get update && sudo apt-get install --no-install-recommends -y python3-c
 sudo -H pip3 install -U colcon-bundle colcon-ros-bundle
 # colcon-bundle setup requirements reinstalls the latest version(50.0.0) of setuptools.
 sudo -H pip3 install -U setuptools!=50.0.0 --force-reinstall
+# temp workaround for setuptools 50.0.0 issue
+export SETUPTOOLS_USE_DISTUTILS=stdlib
 
 COMMON_SA_BUILD_SCRIPT=${SCRIPT_DIR}/common_sa_build.sh
 . "${COMMON_SA_BUILD_SCRIPT}"
