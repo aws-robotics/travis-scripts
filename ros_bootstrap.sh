1a7ea6055a742c7f44db777628686aec34bc371e
Ojas Joshi <36244516+ojasjoshi@users.noreply.github.com>
2020-09-01 15:29:23 -0700

    setuptools 50.0.0 workaround (#73)
    * Remove comment
    
    * Use recommended workaround
    
    * Adding force-reinstall for setuptools installation
    
    * Pin 49.3.0 for setuptools
    
    * Pin <50.0.0 for setuptools
    
    * removing --for-install tag from setuptools
    
    Co-authored-by: Ojas Joshi <ojasjosh@amazon.com>
    Co-authored-by: Anson Wong <wongtsan@amazon.com>

--- ros_bootstrap.sh
+++ ros_bootstrap.sh
@@ -3,7 +3,9 @@ set -xe
 
 # Set up ROS APT and install basic dependencies (rosdep, rosinstall). Must have ROS_VERSION set when called.
 sudo apt-get update && sudo apt-get install -q -y dirmngr curl gnupg2 lsb-release zip python3-pip python3-apt dpkg
-sudo -H pip3 install -U setuptools!=50.0.0
+sudo -H pip3 install -U setuptools
+# NOTE: Workaround for setuptools 50.0.* (see https://github.com/pypa/setuptools/issues/2352)
+export SETUPTOOLS_USE_DISTUTILS=stdlib
 
 if [ "${ROS_VERSION}" == "1" ]; then
   echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros-latest.list
